1a30fbe30143717e02f1928f400415f2deb59cc0
Vidya Vipul <i.vidyavipul@gmail.com>
2026-03-04 17:20:42 +0530

    feat: Add user names, active user list, and typing indicators to server

--- server.js
+++ server.js
@@ -6,23 +6,60 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Store active users
+const activeUsers = new Map();
+
 // Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+// Helper function to get list of active users
+function getActiveUsers() {
+    return Array.from(activeUsers.values());
+}
+
 // The "Connection" Event
 io.on('connection', (socket) => {
-    console.log('A user connected!');
+    console.log('A user connected!', socket.id);
+
+    // Listen for user joining with a username
+    socket.on('user join', (username) => {
+        activeUsers.set(socket.id, { id: socket.id, username });
+        console.log(`${username} joined the chat`);
+        
+        // Notify all clients of the updated user list
+        io.emit('update users', getActiveUsers());
+        // Notify all users about the new user
+        io.emit('user joined', `${username} joined the chat`);
+    });
 
     // Listen for a "chat message" from a user
-    socket.on('chat message', (msg) => {
-        // Send that message to EVERYONE connected
-        io.emit('chat message', msg);
+    socket.on('chat message', (data) => {
+        const user = activeUsers.get(socket.id);
+        if (user) {
+            // Broadcast message with username
+            io.emit('chat message', { username: user.username, message: data });
+        }
+    });
+
+    // Listen for typing events
+    socket.on('typing', (typing) => {
+        const user = activeUsers.get(socket.id);
+        if (user) {
+            socket.broadcast.emit('user typing', { username: user.username, isTyping: typing });
+        }
     });
 
     socket.on('disconnect', () => {
-        console.log('User disconnected');
+        const user = activeUsers.get(socket.id);
+        if (user) {
+            console.log(`${user.username} disconnected`);
+            activeUsers.delete(socket.id);
+            // Notify remaining clients
+            io.emit('update users', getActiveUsers());
+            io.emit('user left', `${user.username} left the chat`);
+        }
     });
 });
 
